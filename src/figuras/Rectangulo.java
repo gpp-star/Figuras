8e13a0ea71f600efc693e6bd3a335e9e21c7b62b
GRACEPP <74842365+gpp-star@users.noreply.github.com>
2023-03-20 17:15:26 -0300

    SE AGREGA CONSTRUCTOR Y ATRIBUTOS

---
+++ src/figuras/Rectangulo.java
@@ -0,0 +1,19 @@
+package figuras;
+
+public class Rectangulo {
+	
+	private double ancho;
+	private double alto;
+	
+	
+	
+	
+	
+	
+
+	public Rectangulo (double ancho, double alto) {
+		this.ancho = ancho;
+		this.alto= alto;
+	}
+
+}
