5c2263e664c172102d4e99b6398cfc0ceac5180c
GRACEPP <74842365+gpp-star@users.noreply.github.com>
2023-03-20 17:57:40 -0300

    SAQUÉ LINEAS VACIAS

--- src/figuras/Rectangulo.java
+++ src/figuras/Rectangulo.java
@@ -4,13 +4,6 @@ public class Rectangulo {
 	
 	private double ancho;
 	private double alto;
-	
-	
-	
-	
-	
-	
-
 	public Rectangulo (double ancho, double alto) {
 		this.ancho = ancho;
 		this.alto= alto;
